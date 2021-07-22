2f636dd025103422fca99dd0d5646f2d125fd9a5
Otavio <ootavio007@hotmail.com>
2021-07-22 16:54:46 -0300

    IMPLEMENTADO CALCULO DE SOMA DAS COLUNAS CM PHP

---
+++ calcular.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$a1 = $_POST['a1'];
+$a2 = $_POST['a2'];
+$a3 = $_POST['a3'];
+$a4 = $_POST['a4'];
+$a5 = $_POST['a5'];
+$a6 = $_POST['a6'];
+$a7 = $_POST['a7'];
+$a8 = $_POST['a8'];
+$a9 = $_POST['a9'];
+
+$b1 = $_POST['b1'];
+$b2 = $_POST['b2'];
+$b3 = $_POST['b3'];
+$b4 = $_POST['b4'];
+$b5 = $_POST['b5'];
+$b6 = $_POST['b6'];
+$b7 = $_POST['b7'];
+$b8 = $_POST['b8'];
+$b9 = $_POST['b9'];
+
+$result1 = $a1+$a2+$a3+$a4+$a5+$a6+$a7+$a8+$a9;
+$result2 = $b1+$b2+$b3+$b4+$b5+$b6+$b7+$b8+$b9;
+
+?>
+
+<h3>O resultado da primeira coluna: <?php echo $result1?></h3>
+<h3>O resultado da segunda coluna: <?php echo $result1?></h3>
+<h3>O resultado da terceira coluna: <?php echo $result1?></h3>
